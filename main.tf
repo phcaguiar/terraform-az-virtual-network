15bc8d2953e8e36a608e97991106bb298c837ed2
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2018-12-05 23:00:08 +0000

    Remove subnets from vnet module

--- main.tf
+++ main.tf
@@ -3,17 +3,7 @@ resource "azurerm_virtual_network" "azure_virtual_network" {
   address_space       = ["${var.vnet_cidr}"]
   location            = "${var.location}"
   resource_group_name = "${var.azure_resource_group}"
-    
-  subnet {
-    name              = "${var.virtual_subnet_name_1}"
-    address_prefix    = "${(cidrsubnet(var.vnet_cidr,8,0))}"
-  }
 
-  subnet {
-    name              = "${var.virtual_subnet_name_2}"
-    address_prefix    = "${(cidrsubnet(var.vnet_cidr,8,1))}"
-  }
-  
   tags {
     team              = "${var.tag_team}"
   }
