65911e1d43b8dfbeaf10f6b2afda5158edcf178e
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2018-12-06 04:38:11 +0000

    adjust id identation

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.name}"
-  id                = "${var.id}"
+  id                  = "${var.id}"
   location            = "${var.location}"
   address_space       = ["${var.address_space}"]
   resource_group_name = "${var.resource_group_name}"
